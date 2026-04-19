40aecc47a9262395a4b38c985d3bad8a21e9545c
indiser <ranabanerjee3000+github@gmail.com>
2026-04-19 12:09:33 +0530

    Update projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,5 +1,5 @@
 export async function getProjects() {
-  const GIST_URL = 'https://gist.githubusercontent.com/indiser/7a264723890c6818f8d7e88b122287da/raw/e4ac49595f4cb910aa778d1e063ec8144d4e630f/projects.json';
+  const GIST_URL = 'https://gist.githubusercontent.com/indiser/7a264723890c6818f8d7e88b122287da/raw/projects.json';
   
   const response = await fetch(GIST_URL, {
     next: { revalidate: 3600 }
