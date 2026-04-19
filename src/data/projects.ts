cb98b1fa95407d6073ad069057466ee45516eab7
indiser <ranabanerjee3000+github@gmail.com>
2026-04-19 12:35:42 +0530

    Change revalidation time for getProjects function
    Updated revalidation period from 3600 seconds to 60 seconds for fetching projects.

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,7 +2,7 @@ export async function getProjects() {
   const GIST_URL = 'https://gist.githubusercontent.com/indiser/7a264723890c6818f8d7e88b122287da/raw/projects.json';
   
   const response = await fetch(GIST_URL, {
-    next: { revalidate: 3600 }
+    next: { revalidate: 60 }
   });
   
   return response.json();
